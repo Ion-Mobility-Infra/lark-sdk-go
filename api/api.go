73c6f06f0a76dc8a815bbbffe87ca29bd3206a60
Calvin Cheng <linchuan.cheng@gmail.com>
2021-05-08 15:39:07 +0800

    Implement and test out an example API call
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- api/api.go
+++ api/api.go
@@ -1,1 +1,9 @@
 package api
+
+const LARKSUITE_OPEN_API = "https://open.larksuite.com/open-apis"
+
+// URLs for API Access Tokens
+const POST_APP_ACCESS_TOKEN = "/auth/v3/app_access_token"
+const POST_APP_ACCESS_TOKEN_INTERNAL = "/auth/v3/app_access_token/internal"
+const POST_TENANT_ACCESS_TOKEN = "/auth/v3/tenant_access_token"
+const POST_TENANT_ACCESS_TOKEN_INTERNAL = "/auth/v3/tenant_access_token/internal"
